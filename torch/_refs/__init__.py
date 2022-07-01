f27c2ae70fd587aad0922246eb93d5907bac5816
lezcano <lezcano-93@hotmail.com>
2022-07-01 14:54:59 +0000

    Implement references for the linalg.norm* functions
    ghstack-source-id: 7f78ae770c2615e952af64ecb7f00d6a5f99f598
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78350

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -1286,7 +1286,7 @@ def sub(
     alpha: Optional[NumberType] = None,
 ):
     """
-    Reference implementation of torch.add
+    Reference implementation of torch.sub
     """
 
     if isinstance(a, Number) and isinstance(b, Number):
